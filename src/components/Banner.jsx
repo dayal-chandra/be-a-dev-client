b6952cb2cb8f41c912394cdf1f0ce34f9fb46564
dayal chandra <dayalchandra511@gmail.com>
2025-07-03 22:53:44 +0600

    react simple typewriter added

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import AutoPlaySlider from "./AutoPlaySlider";
+import { Typewriter } from "react-simple-typewriter";
 
 const Banner = () => {
   return (
@@ -8,8 +9,27 @@ const Banner = () => {
         <h1>
           <span className="text-2xl font-semibold">Learn</span>
           <br />{" "}
-          <span className="text-5xl font-bold text-orange-400">
+          {/* <span className="text-5xl font-bold text-orange-400">
             Web Development
+          </span> */}
+          <span className="text-orange-500 text-5xl font-bold pt-2">
+            <Typewriter
+              words={[
+                "React",
+                "Node.js",
+                "MongoDB",
+                "Express",
+                "Tailwind",
+                "MERN Stack",
+                "Next.js",
+              ]}
+              loop
+              cursor
+              cursorStyle="|"
+              typeSpeed={80}
+              deleteSpeed={50}
+              delaySpeed={1500}
+            />
           </span>
         </h1>
         <p className="text-[18px] pt-2">
