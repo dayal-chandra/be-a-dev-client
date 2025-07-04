4940c79e0ef2725f3bf36ef0af24da79f89b506d
dayal chandra <dayalchandra511@gmail.com>
2025-07-04 14:15:45 +0600

    feature section added and aos implemented

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,39 +1,53 @@
 import React from "react";
+import { FaClipboardList } from "react-icons/fa";
+import { GiProgression } from "react-icons/gi";
+import { IoCalendarNumber } from "react-icons/io5";
+import { TbCertificate } from "react-icons/tb";
 
 const Features = () => {
   return (
     <div className="pt-20">
-      <h1 className="text-3xl text-center font-bold mb-3">Features</h1>
-      <p className="text-center pb-4">
+      <h1 data-aos="fade-up" className="text-3xl text-center font-bold mb-3">
+        How It Works
+      </h1>
+      <p data-aos="fade-up" className="text-center pb-4">
         Explore all features and Master the fundamental of Web Development with
         BeAdev.
       </p>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-        <div className="border border-orange-400 rounded-2xl p-5">
-          <div>hello</div>
+      <div data-aos="fade-up" className="grid grid-cols-1 md:grid-cols-2 gap-5">
+        <div className="border border-orange-400 rounded-2xl p-5 flex flex-col md:flex-row gap-5 justify-start items-center hover:scale-105 transition-transform duration-300 hover:shadow-[0_0_15px_#ffa726]">
           <div>
-            <h1>Learn Everyday</h1>
-            <p>Learn, Practice and stay with the flow to achieve your goal</p>
+            <IoCalendarNumber size={75} />
+          </div>
+          <div>
+            <h1 className="text-2xl font-semibold pb-2">Learn Everyday</h1>
+            <p>Learn, Practice and stay with the flow to achieve your goal.</p>
           </div>
         </div>
-        <div className="border border-orange-400 rounded-2xl p-5">
-          <div>vai</div>
+        <div className="border border-orange-400 rounded-2xl p-5 flex flex-col md:flex-row gap-5 justify-start items-center hover:scale-105 transition-transform duration-300 hover:shadow-[0_0_15px_#ffa726]">
           <div>
-            <h1>Track your progress</h1>
+            <GiProgression size={75} />
+          </div>
+          <div>
+            <h1 className="text-2xl font-semibold pb-2">Track your progress</h1>
             <p>Monitor your progress and stay motivated.</p>
           </div>
         </div>
-        <div className="border border-orange-400 rounded-2xl p-5">
-          <div>ki khobor</div>
+        <div className="border border-orange-400 rounded-2xl p-5 flex flex-col md:flex-row gap-5 justify-start items-center hover:scale-105 transition-transform duration-300 hover:shadow-[0_0_15px_#ffa726]">
           <div>
-            <h1>Test your knowledge</h1>
+            <FaClipboardList size={75} />
+          </div>
+          <div>
+            <h1 className="text-2xl font-semibold pb-2">Test your knowledge</h1>
             <p>Take guesses to evaluate your understanding of material</p>
           </div>
         </div>
-        <div className="border border-orange-400 rounded-2xl p-5">
-          <div>gajkd</div>
+        <div className="border border-orange-400 rounded-2xl p-5 flex flex-col md:flex-row gap-5 justify-start items-center hover:scale-105 transition-transform duration-300 hover:shadow-[0_0_15px_#ffa726]">
           <div>
-            <h1>Get a Certificate</h1>
+            <TbCertificate size={85} />
+          </div>
+          <div>
+            <h1 className="text-2xl font-semibold pb-2">Get a Certificate</h1>
             <p>
               After completing the course we will reward you with the best
               Endorsement.
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,13 @@ import "./index.css";
 import { RouterProvider } from "react-router";
 import { router } from "./routes/Routes.jsx";
 
+import "aos/dist/aos.css";
+import Aos from "aos";
+Aos.init({
+  duration: 500,
+  mirror: true,
+});
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
